f527a4c9203cd19c369ecf2cdf84d3df56e4fff7
Kendrip <69228085+Kendrip@users.noreply.github.com>
2021-02-06 16:28:15 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+# I was bored when i made this hope you like it thou.
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+alphabet_array_upper = list(alphabet)
+print(alphabet_array_upper)
+
+alphabet_array_lower = list(alphabet.lower())
+print(alphabet_array_lower)
